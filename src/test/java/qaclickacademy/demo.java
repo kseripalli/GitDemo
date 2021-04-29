fbfe1e4b1efdfce9201fe0c3ebfe68a856300709
kalpana <seripallikalpana@yahoo.com>
2021-04-29 14:56:28 -0400

    final commit

---
+++ src/test/java/qaclickacademy/demo.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void browserAutomation() {
+		
+		System.out.println("Browser Automation");
+		System.out.println("Web Automation");
+	}
+}
